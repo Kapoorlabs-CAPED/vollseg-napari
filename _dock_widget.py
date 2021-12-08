544c2ed3211cebcfd033c5c3b9507cdeb1697712
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-12-08 19:05:06 +0100

    pyklb

--- _dock_widget.py
+++ _dock_widget.py
@@ -22,6 +22,30 @@ def plugin_wrapper():
     from csbdeep.utils import load_json
     DEBUG = False
     from stardist.models import StarDist2D, StarDist3D
+    from vollseg import SmartSeedPrediction3D, SmartSeedPrediction2D
+    from csbdeep.models import Config, CARE
+    from n2v.models import N2V
+    
+    
+    # get available models
+    _models2d, _aliases2d = get_registered_models(StarDist2D)
+    _models3d, _aliases3d = get_registered_models(StarDist3D)
+    # use first alias for model selection (if alias exists)
+    models2d = [((_aliases2d[m][0] if len(_aliases2d[m]) > 0 else m),m) for m in _models2d]
+    models3d = [((_aliases3d[m][0] if len(_aliases3d[m]) > 0 else m),m) for m in _models3d]
+    
+    _models2d_unet, _aliases2d_unet = get_registered_models(CARE)
+    _models3d_unet, _aliases3d_unet = get_registered_models(CARE)
+    # use first alias for model selection (if alias exists)
+    models2d_unet = [((_aliases2d_unet[m][0] if len(_aliases2d_unet[m]) > 0 else m),m) for m in _models2d_unet]
+    models3d_unet = [((_aliases3d_unet[m][0] if len(_aliases3d_unet[m]) > 0 else m),m) for m in _models3d_unet]
+    
+    model_configs = dict()
+    model_threshs = dict()
+    model_selected = None
+
+    CUSTOM_MODEL = 'CUSTOM_MODEL'
+    model_type_choices = [('2D', SmartSeedPrediction2D), ('3D', SmartSeedPrediction3D), ('Custom 2D/3D', CUSTOM_MODEL)]
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
@@ -31,7 +55,7 @@ def napari_experimental_provide_dock_widget():
 
 @napari_hook_implementation
 def napari_provide_sample_data():
-    from stardist import data
+    from vollseg import data
     return {
         'test_image_cell_2d': {
             'data': lambda: [(data.test_image_cell_2d(), {'name': 'cell2d'})],
