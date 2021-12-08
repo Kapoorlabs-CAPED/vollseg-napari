c3db1c5f3fcd648525646a0792bb2b8ab69536f3
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-12-08 15:57:53 +0100

    'version'

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 15:54:54 2021
+
+@author: rando
+"""
+
+from ._version import __version__
+from ._dock_widget import napari_experimental_provide_dock_widget, napari_provide_sample_data
---
+++ _version.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 15:56:55 2021
+
+@author: rando
+"""
+
+__version__='2021.12.1'
