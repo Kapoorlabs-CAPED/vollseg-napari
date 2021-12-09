b02b2be179ec61bfe5ecd860e5631fbab2835e1b
kapoorlab <randomaccessiblekapoor@gmail.com>
2021-12-09 17:10:10 +0100

    file readers

--- _dock_widget.py
+++ _dock_widget.py
@@ -46,17 +46,13 @@ def plugin_wrapper():
     models2d_unet = [((_aliases2d_unet[m][0] if len(_aliases2d_unet[m]) > 0 else m),m) for m in _models2d_unet]
     models3d_unet = [((_aliases3d_unet[m][0] if len(_aliases3d_unet[m]) > 0 else m),m) for m in _models3d_unet]
     
-    _models2d_denoise_care, _aliases2d_denoise_care = get_registered_models(CARE)
-    _models3d_denoise_care, _aliases3d_denoise_care = get_registered_models(CARE)
+    _models_denoise_care, _aliases_denoise_care = get_registered_models(CARE)
     # use first alias for model selection (if alias exists)
-    models2d_denoise_care = [((_aliases2d_denoise_care[m][0] if len(_aliases2d_denoise_care[m]) > 0 else m),m) for m in _models2d_denoise_care]
-    models3d_denoise_care = [((_aliases3d_denoise_care[m][0] if len(_aliases3d_denoise_care[m]) > 0 else m),m) for m in _models3d_denoise_care]
+    models_denoise_care = [((_aliases_denoise_care[m][0] if len(_aliases_denoise_care[m]) > 0 else m),m) for m in _models_denoise_care]
     
-    _models2d_denoise_n2v, _aliases2d_denoise_n2v = get_registered_models(N2V)
-    _models3d_denoise_n2v, _aliases3d_denoise_n2v = get_registered_models(N2V)
+    _models_denoise_n2v, _aliases_denoise_n2v = get_registered_models(N2V)
     # use first alias for model selection (if alias exists)
-    models2d_denoise_n2v = [((_aliases2d_denoise_n2v[m][0] if len(_aliases2d_denoise_n2v[m]) > 0 else m),m) for m in _models2d_denoise_n2v]
-    models3d_denoise_n2v = [((_aliases3d_denoise_n2v[m][0] if len(_aliases3d_denoise_n2v[m]) > 0 else m),m) for m in _models3d_denoise_n2v]
+    models_denoise_n2v = [((_aliases_denoise_n2v[m][0] if len(_aliases_denoise_n2v[m]) > 0 else m),m) for m in _models_denoise_n2v]
     
     
     model_configs = dict()
@@ -92,7 +88,6 @@ def plugin_wrapper():
                 
                  return model_class(None, name=path_star.name, basedir=str(path_star.parent)), CARE(None, name=path_unet.name, basedir=str(path_unet.parent))
         
-        
         else:
             
             if model_care is not None:
@@ -108,28 +103,70 @@ def plugin_wrapper():
 
     class Output(Enum):
         Labels = 'Label Image'
+        Binary_mask = 'Binary Image'
+        Denoised_image = 'Denoised Image'
         Prob  = 'Probability Map'
         Markers = 'Markers'
         All   = 'All'
-    output_choices = [Output.Labels.value, Output.Prob.value, Output.Markers.value, Output.All.value]  
+    output_choices = [Output.Labels.value, Output.Binary_mask.value, Output.Denoised_image.value, Output.Prob.value, Output.Markers.value, Output.All.value]  
     
     DEFAULTS = dict (
-    model_type     = StarDist2D,
-    model2d        = models2d[0][1],
-    model2d_unet   = models2d_unet[0][1],
-    model3d        = models3d[0][1],
-    model3d_unet   = models3d_unet[0][1],
-    norm_image     = True,
-    perc_low       =  1.0,
-    perc_high      = 99.8,
-    norm_axes      = 'ZYX',
-    prob_thresh    = 0.5,
-    nms_thresh     = 0.4,
-    output_type    = Output.All.value,
-    n_tiles        = 'None'
-)                  
+            seg_model_type = SmartSeedPrediction3D,
+            den_model_type = N2V,
+            model2d_star        = models2d[0][1],
+            model2d_unet   = models2d_unet[0][1],
+            model3d_star        = models3d[0][1],
+            model3d_unet   = models3d_unet[0][1],
+            model_denoise_n2v  = models_denoise_n2v[0][1],
+            model_denoise_care  = models_denoise_care[0][1],
+            norm_image     = True,
+            perc_low       =  1.0,
+            perc_high      = 99.8,
+            norm_axes      = 'ZYX',
+            prob_thresh    = 0.5,
+            nms_thresh     = 0.4,
+            output_type    = Output.All.value,
+            n_tiles        = 'None'
+    )                  
      
+    logo = abspath(__file__, 'resources/vollseg_logo_napari.png')
 
+    @magicgui (
+        label_head      = dict(widget_type='Label', label=f'<h1><img src="{logo}">VollSeg</h1>'),
+        image           = dict(label='Input Image'),
+        axes            = dict(widget_type='LineEdit', label='Image Axes'),
+        label_nn        = dict(widget_type='Label', label='<br><b>Neural Network Prediction:</b>'),
+        seg_model_type  = dict(widget_type='RadioButtons', label='Seg Model Type', orientation='horizontal', choices=seg_model_type_choices, value=DEFAULTS['seg_model_type']),
+        den_model_type  = dict(widget_type='RadioButtons', label='Den Model Type', orientation='horizontal', choices=den_model_type_choices, value=DEFAULTS['den_model_type']),
+        model2d_star    = dict(widget_type='ComboBox', visible=False, label='Pre-trained StarDist Model', choices=models2d, value=DEFAULTS['model2d']),
+        model3d_star    = dict(widget_type='ComboBox', visible=False, label='Pre-trained StarDist Model', choices=models3d, value=DEFAULTS['model3d']),
+        
+        model2d_unet    = dict(widget_type='ComboBox', visible=False, label='Pre-trained UNET Model', choices=models2d_unet, value=DEFAULTS['model2d_unet']),
+        model3d_unet    = dict(widget_type='ComboBox', visible=False, label='Pre-trained UNET Model', choices=models3d_unet, value=DEFAULTS['model3d_unet']),
+        
+        model_denoise_n2v   = dict(widget_type='ComboBox', visible=False, label='Pre-trained N2V Model', choices=models_denoise_n2v, value=DEFAULTS['model_denoise_n2v']), 
+        model_denoise_care   = dict(widget_type='ComboBox', visible=False, label='Pre-trained CARE Model', choices=models_denoise_care, value=DEFAULTS['model_denoise_care']),
+        
+        model_folder    = dict(widget_type='FileEdit', visible=False, label='Custom Model', mode='d'),
+        model_axes      = dict(widget_type='LineEdit', label='Model Axes', value=''),
+        norm_image      = dict(widget_type='CheckBox', text='Normalize Image', value=DEFAULTS['norm_image']),
+        label_nms       = dict(widget_type='Label', label='<br><b>NMS Postprocessing:</b>'),
+        perc_low        = dict(widget_type='FloatSpinBox', label='Percentile low',              min=0.0, max=100.0, step=0.1,  value=DEFAULTS['perc_low']),
+        perc_high       = dict(widget_type='FloatSpinBox', label='Percentile high',             min=0.0, max=100.0, step=0.1,  value=DEFAULTS['perc_high']),
+        norm_axes       = dict(widget_type='LineEdit',     label='Normalization Axes',                                         value=DEFAULTS['norm_axes']),
+        prob_thresh     = dict(widget_type='FloatSpinBox', label='Probability/Score Threshold', min=0.0, max=  1.0, step=0.05, value=DEFAULTS['prob_thresh']),
+        nms_thresh      = dict(widget_type='FloatSpinBox', label='Overlap Threshold',           min=0.0, max=  1.0, step=0.05, value=DEFAULTS['nms_thresh']),
+        output_type     = dict(widget_type='ComboBox', label='Output Type', choices=output_choices, value=DEFAULTS['output_type']),
+        label_adv       = dict(widget_type='Label', label='<br><b>Advanced Options:</b>'),
+        n_tiles         = dict(widget_type='LiteralEvalLineEdit', label='Number of Tiles', value=DEFAULTS['n_tiles']),
+        timelapse_opts  = dict(widget_type='ComboBox', label='Time-lapse Labels ', choices=timelapse_opts, value=DEFAULTS['timelapse_opts']),
+        set_thresholds  = dict(widget_type='PushButton', text='Set optimized postprocessing thresholds (for selected model)'),
+        defaults_button = dict(widget_type='PushButton', text='Restore Defaults'),
+        progress_bar    = dict(label=' ', min=0, max=0, visible=False),
+        layout          = 'vertical',
+        persist         = True,
+        call_button     = True,
+    )
 
 def inrimage_file_reader(path):
    array = inrimage.read_inrimage(path)
@@ -160,7 +197,6 @@ def h5image_file_reader(path):
 def napari_get_reader(path):
    # If we recognize the format, we return the actual reader function
    if isinstance(path, str) and path.endswith(".inr"):
-      
       return inrimage_file_reader
    if isinstance(path, str) and path.endswith(".klb"):
       return klbimage_file_reader
