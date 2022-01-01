825e3595a7e20a4cd7e27a776afa4d53dd82b999
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-01-01 13:33:47 +0100

    test case

--- vollseg_napari/tests/example_napari.py
+++ vollseg_napari/tests/example_napari.py
@@ -7,15 +7,28 @@ def show_napari_3d():
     viewer =  napari.Viewer()
     viewer.add_image(x)
     viewer.window.add_plugin_dock_widget('VollSeg')
+    
+    
 def show_napari_2d():
     x = test_image_nuclei_2d()
     viewer =  napari.Viewer()
     viewer.add_image(x)
     viewer.window.add_plugin_dock_widget('VollSeg')
 
-
+def show_napari_3d_test():
+    x = imread("/home/varunkapoor/Downloads/Testdatasets/Nuclei3D-2.tif")
+    viewer =  napari.Viewer()
+    viewer.add_image(x)
+    viewer.window.add_plugin_dock_widget('TestVollSeg')
+    
+    
+def show_napari_2d_test():
+    x = test_image_nuclei_2d()
+    viewer =  napari.Viewer()
+    viewer.add_image(x)
+    viewer.window.add_plugin_dock_widget('TestVollSeg')
 
 if __name__ == '__main__':
-    viewer = show_napari_3d()
+    viewer = show_napari_2d_test()
   
     napari.run()
