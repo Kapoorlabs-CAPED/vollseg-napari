e7114717bbd16541ff84ebadfe3e74c84630d190
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-02-03 14:38:30 +0100

    cov

--- vollseg_napari/_tests/test_napari.py
+++ vollseg_napari/_tests/test_napari.py
@@ -1,5 +1,6 @@
 from vollseg_napari import napari_get_reader
 import numpy as np
+from tifffile import imwrite
 import pytest
 
 @pytest.fixture
@@ -8,33 +9,29 @@ def write_results(tmp_path):
 
     def write_func(filename):
 
-       test_file = str(tmp_path/filename)
-       original_data = np.random.rand(20,20,20)
-       np.save(test_file, original_data)
+       test_file, original_data, reader = test_get_reader_returns_callable(tmp_path, filename)
        
-       return test_file, original_data
+       return test_file, original_data, reader
 
-       
+
     return write_func
 #tmp_path is a pytest fixture    
 def test_get_reader_returns_callable(tmp_path, filename = 'file.tif'):
      
        test_file = str(tmp_path/filename)
        original_data = np.random.rand(20,20,20)
-       np.save(test_file, original_data)
        reader = napari_get_reader(test_file)
+       imwrite(test_file, original_data)
        assert callable(reader), f'{reader} is not a valid file to be read by this function' 
 
-       return test_file, original_data
+       return test_file, original_data, reader
 
 
 def test_reader_round_trip(write_results):
 
 
-           test_file, original_data = write_results("file.tif")
+           test_file, original_data, reader = write_results("file.tif")
 
-           reader = napari_get_reader(test_file)
-           assert callable(reader), f'{reader} is not a valid file to be read by this function'
            layer_data_list = reader(test_file)
 
            assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
