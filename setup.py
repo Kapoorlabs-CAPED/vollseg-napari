50b4e93675b5357a17eb4c84aec78cd99501a576
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-02-09 17:45:43 +0100

    mode

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(_dir,'README.md'), encoding="utf-8") as f:
 setup(
     name='vollseg-napari',
 
-    version= '2.0.9',
+    version= '2.1.0',
 
     description='Irregular cell shape segmentation using VollSeg',
     long_description=long_description,
