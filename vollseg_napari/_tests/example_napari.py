21507464fbe2cae11c2a702dd188cb9cf8ce0687
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-04-06 17:13:20 +0200

    version

---
+++ vollseg_napari/_tests/example_napari.py
@@ -0,0 +1,14 @@
+import napari
+from tifffile import imread
+
+def show_napari_3d():
+    viewer =  napari.Viewer()
+    viewer.window.add_plugin_dock_widget('vollseg-napari','VollSeg')
+    
+    
+ 
+if __name__ == '__main__':
+    viewer = show_napari_3d()
+  
+    napari.run()
+    
