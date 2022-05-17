85237e1511c2a5e555bea8e297ff254b3b3d7dc5
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-05-17 16:33:28 +0200

    add [test]

--- vollseg_napari/_tests/test_open_sample_models.py
+++ vollseg_napari/_tests/test_open_sample_models.py
@@ -140,6 +140,56 @@ def test_default_star_roi_XY(make_napari_viewer):
     fake_plugin_extra_parameters.unet_model_axes.value = 'YX'
     valid_roi = update_quad(fake_plugin_star_parameters,model_roi,twod_image )
     assert valid_roi == True
+def test_default_roi_XY(make_napari_viewer):
+
+
+    fake_plugin_master = _test_dock_widget.plugin_wrapper_vollseg()
+    fake_plugin, fake_plugin_star_parameters, fake_plugin_extra_parameters, fake_plugin_display_parameters,  fake_plugin_stop_parameters, get_data = fake_plugin_master
+    fake_viewer = make_napari_viewer()
+    #get a slice in time and it is a TZYX shape
+    image = vollseg.get_data.get_test_data()
+    image = np.asarray(vollseg.get_data.get_test_data()[0:2,image.shape[1] - 5:image.shape[1] + 5, image.shape[2] - 5:image.shape[2] + 5, image.shape[3] - 5:image.shape[3] + 5])
+    threed_image = image[0,:]
+    twod_image = threed_image[0,:]
+    name = 'test_2d'
+    fake_viewer.add_image(twod_image, name = name )
+    
+    #Test the pre-trained models if they are compatiable with the images they are supposed to work on 
+    key_mask = fake_plugin.roi_model_type.value, fake_plugin.model_roi.value    
+    fake_plugin.roi_model_type.value = MASKUNET    
+    fake_plugin.model_roi.value = 'Xenopus_Cell_Tissue_Segmentation'
+    model_path = vollseg.get_data.get_maskunet_modelpath()
+    model_roi = fake_plugin.roi_model_type.value(None, name=fake_plugin.model_roi.value, basedir=str(model_path))
+    fake_plugin_star_parameters.n_tiles.value = (1,2,2)
+    fake_plugin_extra_parameters.unet_model_axes.value = 'YX'
+    valid_roi = update_2D_roi(fake_plugin_star_parameters,model_roi,twod_image )
+    assert valid_roi == True
+
+def test_default_unet_roi_XY(make_napari_viewer):
+
+
+    fake_plugin_master = _test_dock_widget.plugin_wrapper_vollseg()
+    fake_plugin, fake_plugin_star_parameters, fake_plugin_extra_parameters, fake_plugin_display_parameters,  fake_plugin_stop_parameters, get_data = fake_plugin_master
+    fake_viewer = make_napari_viewer()
+    #get a slice in time and it is a TZYX shape
+    image = vollseg.get_data.get_test_data()
+    image = np.asarray(vollseg.get_data.get_test_data()[0:2,image.shape[1] - 5:image.shape[1] + 5, image.shape[2] - 5:image.shape[2] + 5, image.shape[3] - 5:image.shape[3] + 5])
+    threed_image = image[0,:]
+    twod_image = threed_image[0,:]
+    name = 'test_2d'
+    fake_viewer.add_image(twod_image, name = name )
+    
+    #Test the pre-trained models if they are compatiable with the images they are supposed to work on 
+    key_mask = fake_plugin.roi_model_type.value, fake_plugin.model_roi.value    
+    fake_plugin.roi_model_type.value = MASKUNET    
+    fake_plugin.model_roi.value = 'Xenopus_Cell_Tissue_Segmentation'
+    model_path = vollseg.get_data.get_maskunet_modelpath()
+    model_roi = fake_plugin.roi_model_type.value(None, name=fake_plugin.model_roi.value, basedir=str(model_path))
+    fake_plugin_star_parameters.n_tiles.value = (1,2,2)
+    fake_plugin_extra_parameters.unet_model_axes.value = 'YX'
+    unet_model = model_roi
+    valid_roi = update_2D_unet_roi(fake_plugin_star_parameters, unet_model, model_roi,twod_image )
+    assert valid_roi == True    
 
 def test_default_star_roi_XYZ(make_napari_viewer):
 
@@ -243,3 +293,25 @@ def update_poly(fake_plugin_star_parameters, roi_model, image ):
       valid = False
 
     return valid     
+
+def update_2D_roi(fake_plugin_star_parameters, roi_model, image ):
+
+    res = VollSeg(image, roi_model = roi_model, n_tiles = fake_plugin_star_parameters.n_tiles.value, axes = 'YX')
+  
+    if len(res) == 3:
+      valid = True
+    else:
+      valid = False
+
+    return valid    
+
+def update_2D_unet_roi(fake_plugin_star_parameters, unet_model, roi_model, image ):
+
+    res = VollSeg(image, unet_model = unet_model, roi_model = roi_model, n_tiles = fake_plugin_star_parameters.n_tiles.value, axes = 'YX')
+  
+    if len(res) == 3:
+      valid = True
+    else:
+      valid = False
+
+    return valid    
