e8bc0291f96f1b1c9c56bd600b67f07aa5dd2654
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-09-15 20:28:19 +0200

    add simple nuclei segmentation script

---
+++ scripts/create_training_patches.py
@@ -0,0 +1,45 @@
+from vollseg import SmartPatches
+
+
+def main( ):
+
+
+        base_membrane_dir =  'base_membrane_dir' 
+        raw_membrane_dir = 'raw_membrane_dir'
+        base_nuclei_dir =  'base_nuclei_dir' 
+        raw_nuclei_dir = 'raw_nuclei_dir'
+        
+
+        membrane_channel_results_directory = 'membrane_channel_results_directory'
+        membrane_raw_save_dir = 'membrane_raw_save_dir'
+        membrane_real_mask_patch_dir = 'membrane_real_mask_patch_dir'
+        membrane_binary_mask_patch_dir = 'membrane_binary_mask_patch_dir'
+        nuclei_channel_results_directory = 'nuclei_channel_results_directory'
+        nuclei_raw_save_dir = 'nuclei_raw_save_dir'
+        nuclei_real_mask_patch_dir = 'nuclei_real_mask_patch_dir'
+        nuclei_binary_mask_patch_dir = 'nuclei_binary_mask_patch_dir'
+        lower_ratio_fore_to_back=0.1
+        upper_ratio_fore_to_back=0.9
+        patch_size = (8, 128, 128)
+        erosion_iterations = 0
+        create_for_channel = 'nuclei'
+
+        
+        SmartPatches(base_membrane_dir,
+        raw_membrane_dir,
+        base_nuclei_dir,
+        raw_nuclei_dir,
+        nuclei_channel_results_directory,
+        membrane_channel_results_directory,
+        nuclei_raw_save_dir,
+        membrane_raw_save_dir,
+        nuclei_real_mask_patch_dir,
+        membrane_real_mask_patch_dir,
+        nuclei_binary_mask_patch_dir,
+        membrane_binary_mask_patch_dir, patch_size, erosion_iterations,
+        create_for_channel = create_for_channel, 
+        lower_ratio_fore_to_back=lower_ratio_fore_to_back,
+        upper_ratio_fore_to_back=upper_ratio_fore_to_back)
+
+if __name__=='__main__':
+        main()        
